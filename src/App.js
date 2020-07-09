bb2a69234f0cc5a07d4d6d75be1c0dcbed5f1046
NexusX45 <mszsamip@gmail.com>
2020-07-09 14:10:14 +0530

    Carousel Update

--- src/App.js
+++ src/App.js
@@ -17,18 +17,40 @@ function App() {
       <span className="lead font-weight-normal">their social media marketing with animated videos.</span>
       <br />
       <br />
-
-      <button className="btn btn-primary">Start a Project</button>
-    
-    <div className="carousel slide" data-ride= "carousel">
-      <ol className="carousel-indicators">
-        <li data-target = "#" className="active"></li>
-        <li></li>
-        <li></li>
-
-      </ol>
-
-    </div>
+      <button className="btn btn-primary" style={{backgroundColor:"#FF784B", border:"none"}}>Start a Project</button>
+      <br />
+      <br />
+      <a className="lead font-weight-normal" style={{padding:"2rem"}}>Grocery</a>
+      <a className="lead font-weight-normal" style={{padding:"2rem"}}>Restaurant</a>
+      <a className="lead font-weight-normal" style={{padding:"2rem"}}>Spa & Salon</a>
+      <a className="lead font-weight-normal" style={{padding:"2rem"}}>Retail</a>
+      <a className="lead font-weight-normal" style={{padding:"2rem"}}>Home Decor</a>
+      <br />
+      <br />
+      <center>
+        <div style={{ height:"1rem", width:"50rem"}}>
+          <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+          </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg" class="d-block w-100" alt="#"/>
+              </div>
+              <div class="carousel-item">
+                <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg" class="d-block w-100" alt="#"/>
+              </div>
+              <div class="carousel-item">
+                <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg" class="d-block w-100" alt="#"/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </center>
+      
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -17,9 +17,9 @@ function Navbar(){
   <div>
     <nav className="site-header stickly-top py-1">
       <div class="container d-flex flex-column flex-md-row justify-content-between">
-        <a class="py-2" href="#" aria-label="Product">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mx-auto" role="img" viewBox="0 0 24 24" focusable="false"><title>Tweencraft</title><circle cx="12" cy="12" r="10"></circle><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"></path></svg>
-          <span>Tweencraft</span>
+        <a class="py-2" href="#" aria-label="Product" style={{textDecoration:"none"}}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="#FF784B" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mx-auto" role="img" viewBox="0 0 24 24" focusable="false"><title>Tweencraft</title><circle cx="12" cy="12" r="10"></circle><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"></path></svg>
+          <span style={{color:"#FF784B"}}>Tweencraft</span>
         </a>
         <a className="py-2 d-none d-md-inline-block text-dark" href="#">Samples</a> 
         <a className="py-2 d-none d-md-inline-block text-dark" href="#">How it Works</a>
